58a6723ee86b5cee758398ea8dbf57d2e40152a1
TakaiItsuki <17095@hakodate.kosen-ac.jp>
2021-10-21 12:07:06 +0900

    Dがキャラ移動処理を追加

--- game1/D.java
+++ game1/D.java
@@ -14,9 +14,6 @@ public class D extends Actor
      */
     public void act() 
     {
-        if( Greenfoot.isKeyDown( "left" ) ){
-            setRotation(180);
-            move(1);
-        }
-    }    
+        // Add your action code here.
+    }      
 }
