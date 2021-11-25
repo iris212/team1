09fb5db6ec0ef2a004a96c2478621c42bb3c13d8
ohmi461 <17030@hakodate.kosen-ac.jp>
2021-11-25 11:42:56 +0900

    Bがローカル更新

--- game1/MyWorld.java
+++ game1/MyWorld.java
@@ -14,7 +14,8 @@ public class MyWorld extends World
      * 
      */
     public MyWorld()
-    {    
+    {   
+      
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(850, 550, 1);
         int map[][] = {
